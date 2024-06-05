c9384db0c593a8c862432b3feae35ef89c36449b
Kingkaspev <kingsleychiboy22@gmail.com>
2024-06-06 00:18:03 +0100

    DbContext class

---
+++ BookBarn.API/BookBarn.Infrastructure/Context/BookBarnDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookBarn.Domain.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BookBarn.Infrastructure.Context
+{
+    public class BookBarnDbContext : IdentityDbContext<AppUser>
+    {
+        public BookBarnDbContext(DbContextOptions<BookBarnDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
--- BookBarn.API/BookBarn/Extentions/DIServiceExtension.cs
+++ BookBarn.API/BookBarn/Extentions/DIServiceExtension.cs
@@ -1,4 +1,5 @@
 ﻿using BookBarn.Domain.Entities;
+using BookBarn.Infrastructure.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Principal;
@@ -10,11 +11,11 @@ namespace BookBarn.Extentions
         public static void AddDependencies(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<RoleManager<IdentityRole>>();
-            //services.AddDbContext<BookBarnDbContext>(options =>
-            //options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
-            //services.AddIdentity<AppUser, IdentityRole>()
-            //.AddEntityFrameworkStores<BookBarnDbContext>()
-            //.AddDefaultTokenProviders();
+            services.AddDbContext<BookBarnDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<AppUser, IdentityRole>()
+            .AddEntityFrameworkStores<BookBarnDbContext>()
+            .AddDefaultTokenProviders();
 
             services.AddCors(options =>
             {
