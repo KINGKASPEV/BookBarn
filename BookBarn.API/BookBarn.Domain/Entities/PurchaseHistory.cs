dcefef213d1da7a6fe982b2425896b51ef56fe6e
Kingkaspev <kingsleychiboy22@gmail.com>
2024-06-08 07:44:11 +0100

    Cart endpoints implemented

--- BookBarn.API/BookBarn.Domain/Entities/PurchaseHistory.cs
+++ BookBarn.API/BookBarn.Domain/Entities/PurchaseHistory.cs
@@ -4,7 +4,7 @@
     {
         public string AppUserId { get; set; }
         public AppUser AppUser { get; set; }
-        public List<Book> Books { get; set; }
+        public ICollection<Book> Books { get; set; }
         public DateTime PurchaseDate { get; set; } = DateTime.Now;
     }
 }
--- BookBarn.API/BookBarn/Controllers/CartController.cs
+++ BookBarn.API/BookBarn/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookBarn.Application.DTOs.Cart;
+using BookBarn.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookBarn.Controllers
 {
@@ -6,5 +8,74 @@ namespace BookBarn.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCart([FromBody] CreateCartDto createCartDto)
+        {
+            var response = await _cartService.AddCartAsync(createCartDto);
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpPost("{cartId}/books/{bookId}")]
+        public async Task<IActionResult> AddBookToCart(string cartId, string bookId)
+        {
+            var response = await _cartService.AddBookToCartAsync(cartId, bookId);
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpDelete("{cartId}")]
+        public async Task<IActionResult> DeleteCart(string cartId)
+        {
+            var response = await _cartService.DeleteCartAsync(cartId);
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCarts()
+        {
+            var response = await _cartService.GetAllCartsAsync();
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpGet("{cartId}")]
+        public async Task<IActionResult> GetCartById(string cartId)
+        {
+            var response = await _cartService.GetCartByIdAsync(cartId);
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpDelete("{cartId}/books/{bookId}")]
+        public async Task<IActionResult> RemoveBookFromCart(string cartId, string bookId)
+        {
+            var response = await _cartService.RemoveBookFromCartAsync(cartId, bookId);
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCart([FromBody] UpdateCartDto updateCartDto)
+        {
+            var response = await _cartService.UpdateCartAsync(updateCartDto);
+            if (response.Succeeded)
+                return Ok(response);
+            return BadRequest(response);
+        }
     }
 }
